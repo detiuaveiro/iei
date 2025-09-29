e44f6c8de30f1caa3c0dee1a341e862a09c9c0ba
Mario Antunes <mariolpantunes@gmail.com>
2025-09-29 01:50:18 +0100

    Update folder structure

--- classes/class_00/02 support/vm.sh
+++ classes/class_00/02 support/vm.sh
@@ -1,5 +1,30 @@
 #!/usr/bin/env bash
 
+if [ "$1" = "arm" ]; then
+echo -e "Start ARM flavor..."
+
+qemu-system-aarch64 \
+-m 4G \
+-machine type=virt \
+-cpu max \
+-m 4g \
+-smp 4 \
+-initrd "./initrd.gz" \
+-kernel "./vmlinuz" \
+-append "console=ttyAMA0" \
+-drive file=./debian-13.1.0-arm64-netinst.iso,id=cdrom,if=none,media=cdrom \
+  -device virtio-scsi-device \
+  -device scsi-cd,drive=cdrom \
+-drive file="./vm_arm.qcow2",id=hd,if=none,media=disk \
+  -device virtio-scsi-device \
+  -device scsi-hd,drive=hd \
+-netdev user,id=net0,hostfwd=tcp::5555-:22 \
+  -device virtio-net-device,netdev=net0 \
+-nographic
+
+else
+
+echo -e "Start AMD64 flavor..."
 qemu-system-x86_64 \
 -enable-kvm \
 -cpu host \
@@ -9,3 +34,7 @@ qemu-system-x86_64 \
 -vga virtio \
 -drive file=vm.qcow2 \
 -boot c
+
+fi
+
+echo -e "Done."
